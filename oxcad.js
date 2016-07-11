9eea646251dde4359da89053a871637ba1bcbf10
Bill Ola Rasmussen <billra@gmail.com>
2016-07-11 10:59:50 +0200

    bump version

--- oxcad.js
+++ oxcad.js
@@ -211,7 +211,7 @@ function logMsg() {
 }
 
 function logClear() {
-	logEdit.setValue('OxCad v0.11, Log Entries:\n');
+	logEdit.setValue('OxCad v0.12, Log Entries:\n');
 	logEdit.clearSelection();
 }
 
