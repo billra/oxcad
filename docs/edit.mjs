1849b307d75d5261c2851f750678dd511deec2d9
Bill Ola Rasmussen <billra@gmail.com>
2025-06-02 07:35:13 +0200

    text version of examples, pt.1

--- docs/edit.mjs
+++ docs/edit.mjs
@@ -1,6 +1,37 @@
 import './acewrap.mjs';
 
-// -=-=-=-=-=- Ace Code Window (User Code Editing) -=-=-=-=-=-=-
+// --- load code examples ---
+function parseKeyValueText(txt) {
+    // normalize line endings
+    txt = txt.replace(/\r\n?/g, '\n');
+
+    const pattern = new RegExp(
+        [
+            '(?:^|\\n)',            // start of file or after a newline
+            'K:\\s*',               // 'K:' key marker, optional whitespace
+            '(?<key>.*?)',          // non-greedy capture for key
+            '\\s*\\n',              // optional whitespace, newline
+            'V:\\s*\\n',            // 'V:', optional whitespace, newline
+            '(?<value>[\\s\\S]*?)', // non-greedy value, anything including newlines
+            '(?=(?:\\nK:)|$)'       // Lookahead for next "\nK:" or end of string
+        ].join(''), 'g'
+    );
+
+    const result = {};
+
+    for (const match of txt.matchAll(pattern)) {
+        const { key, value } = match.groups;
+        result[key] = value;
+    }
+
+    return result;
+}
+
+const response = await fetch('./examples.txt');
+const responseText = await response.text();
+const codeExamples = parseKeyValueText(responseText);
+
+// --- code editing ---
 
 export function getCodeEdit() { return codeEdit; }
 
@@ -25,13 +56,13 @@ export function runCode() {
 }
 
 export function exampleChangeFunc(id) {
-    codeEdit.setValue(window.codeExamples[id]);
+    codeEdit.setValue(codeExamples[id]);
     codeEdit.clearSelection();
 }
 
 export function fillExampleDropdown(selectedKey) {
     const select = document.getElementById("selectBox");
-    for (const key in window.codeExamples) {
+    for (const key in codeExamples) {
         const el = document.createElement("option");
         el.textContent = key;
         el.value = key;
