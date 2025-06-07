e08d16ceb5f2353b6279cd5d2a872fe721832a74
Bill Ola Rasmussen <billra@gmail.com>
2025-06-07 06:26:49 +0200

    rewrite in progress, code complete

--- docs/ox.mjs
+++ docs/ox.mjs
@@ -1,6 +1,16 @@
-// ['a', 'b', 'c'], [1, 2] -> ['a', 1, 'b', 2, 'c']
-export function interleave(a, b) {
-    return a.flatMap((x, i) => i < b.length ? [x, b[i]] : [x]);
+// --- utilities ---
+// JavaScript silently accepts missing parameters.
+// Use default parameters to throw required parameter errors.
+function required(name) {
+    throw new Error(`missing '${name}' parameter`);
+}
+
+function assert(
+    condition = required('condition'),
+    message = required('message')) {
+    if (!condition) {
+        throw new Error(message);
+    }
 }
 
 // Call function with a range of values, produce an array of results. Each parameter
@@ -15,6 +25,8 @@ export function callRange(count, func, ...rest) {
         }))
     );
 }
+
+// --- generate SVG ---
 
 export function surface(x, y, edge1, edge2, color = 'var(--svg-stroke)', width = '1pt') {
     // todo: center drawing on nearest major grid line
@@ -32,72 +44,91 @@ export function path(x, y, objs, color = 'var(--svg-stroke)', width = '1pt') {
     return str;
 }
 
-// return the entire edge length of the drawing element
-export function edgeLength(objs) {
-    return objs.reduce((sum, obj) => sum + obj.edgeLen, 0);
-}
+// --- classes ---
 
-// return the end position of the drawing element relative to the start
-export function endPoint(objs) {
-    return objs.reduce(
-        (sum, obj) => ({ x: sum.x + obj.x, y: sum.y + obj.y }),
-        { x: 0, y: 0 }
-    );
-}
-
-export function reflect(objs, mirrorAngleDeg = 90) { // mirror default Y axis
-    const tail = clone(objs.slice(0, -1).reverse(), 1, mirrorAngleDeg); // no reflection on center item
-    return objs.concat(tail);
-}
-
-// mirror angle across axis
-function mirror(angle, axis) {
-  return 2 * axis - angle;
+class Container {
+    #items;
+    constructor(items) {
+        this.#items = items;
+    }
+    // return the entire edge length of the drawing element
+    edgeLength() {
+        return this.#items.reduce((sum, obj) => sum + obj.edgeLen, 0);
+    }
+    // return the end position of the drawing element relative to the start
+    endPoint() {
+        return this.#items.reduce(
+            (sum, item) => ({ x: sum.x + item.x, y: sum.y + item.y }),
+            { x: 0, y: 0 }
+        );
+    }
+    // ['a', 'b', 'c'], [1, 2] -> ['a', 1, 'b', 2, 'c']
+    interleave(rhs) {
+        assert(this.#items.length - 1 === rhs.length, `bad lengths: ${this.#items.length}, ${rhs.length}`);
+        return new Container(this.#items.flatMap((item, i) => i < rhs.length ? [item, rhs[i]] : [item]));
+    }
+    scale(factor) {
+        return new Container(this.#items.map(item => item.scale(factor)));
+    }
+    mirror(axis) {
+        return new Container(this.#items.map(item => item.mirror(axis)));
+    }
+    reflect(axis) {
+        // -1 -> last item is not reflected
+        const items = this.#items.slice(0, -1).reverse(); // collect tail items
+        const tail = items.map(item => item.mirror(axis)); // reverse each tail item
+        return new Container(this.#items.concat(tail));
+    }
 }
 
 function radians(degrees) {
     return degrees * Math.PI / 180;
 }
 
-function move(angleDeg, length, from = { x: 0, y: 0 }) {
-    const angleRad = radians(angleDeg);
+function move(angle, length, from = { x: 0, y: 0 }) {
+    const rad = radians(angle);
     return {
-        x: length * Math.cos(angleRad) + from.x,
-        y: length * Math.sin(angleRad) + from.y
+        x: length * Math.cos(rad) + from.x,
+        y: length * Math.sin(rad) + from.y
     };
 }
 
+// mirror angle across axis
+function mirrorAngle(angle, axis) {
+    return 2 * axis - angle;
+}
+
 class Edge {
-    constructor(angleDeg, length) {
-        const end = move(angleDeg, length);
+    constructor(angle, length) {
+        const end = move(angle, length);
         this.part = `l${end.x},${end.y}`;
         this.edgeLen = length;
-        this.scale = factor => new Edge(angleDeg, length * factor);
-        this.mirror = axis => new Edge(mirror(angleDeg, axis), length);
+        this.scale = factor => new Edge(angle, length * factor);
+        this.mirror = axis => new Edge(mirrorAngle(angle, axis), length);
         this.x = end.x;
         this.y = end.y;
     }
 }
 
 export function makeEdge(
-    angleDeg = required('angleDeg'),
+    angle = required('angle'),
     length = required('length')) {
-    return new Edge(angleDeg, length);
+    return new Edge(angle, length);
 }
 
 // todo: special case smooth <= 0 and smooth >=1 with fewer segments
 class Notch {
-    constructor(angleDeg, angleOpenDeg, length, smooth) {
+    constructor(angle, openAngle, length, smooth) {
         const resLen = length / (1 + smooth); // make overall length the same for straight and smooth notches
-        const halfAngleOpenDeg = angleOpenDeg / 2;
-        const a12 = angleDeg - halfAngleOpenDeg;
-        const a56 = angleDeg + halfAngleOpenDeg - 180;
+        const halfOpenAngle = openAngle / 2;
+        const a12 = angle - halfOpenAngle;
+        const a56 = angle + halfOpenAngle - 180;
         const lenV = resLen * (1 - smooth);
         const lenB = resLen * smooth;
         const m1 = move(a12, lenV);
         const m2 = move(a12, lenB);
-        const m3 = move(angleDeg, lenB, m2);
-        const m4 = move(angleDeg - 180, lenB);
+        const m3 = move(angle, lenB, m2);
+        const m4 = move(angle - 180, lenB);
         const m5 = move(a56, lenB, m4);
         const m6 = move(a56, lenV);
         const end = endPoint([m1, m3, m5, m6]);
@@ -107,36 +138,17 @@ class Notch {
             `q${m4.x},${m4.y} ${m5.x},${m5.y}` +
             `l${m6.x},${m6.y}`;
         this.edgeLen = 0;
-        this.scale = factor => new Notch(angleDeg, angleOpenDeg, length * factor, smooth);
-        this.mirror = axis => new Notch(mirror(angleDeg, axis), angleOpenDeg, length, smooth);
+        this.scale = factor => new Notch(angle, openAngle, length * factor, smooth);
+        this.mirror = axis => new Notch(mirrorAngle(angle, axis), openAngle, length, smooth);
         this.x = end.x;
         this.y = end.y;
     }
 }
 
 export function makeNotch(
-    angleDeg = required('angleDeg'),
-    angleOpenDeg = required('angleOpenDeg'),
+    angle = required('angle'),
+    openAngle = required('openAngle'),
     length = required('length'),
     smooth = required('smooth')) {
-    return new Notch(angleDeg, angleOpenDeg, length, smooth);
-}
-
-// JavaScript silently accepts missing parameters.
-// Use default parameters to throw required parameter errors.
-function required(name) {
-    throw new Error(`missing '${name}' parameter`);
-}
-
-class Container {
-    #items;
-    constructor(items) {
-        this.#items = items;
-    }
-    scale(factor) {
-        return new Container(this.#items.map(item => item.scale(factor)));
-    }
-    mirror(axis) {
-        return new Container(this.#items.map(item => item.mirror(axis)));
-    }
+    return new Notch(angle, openAngle, length, smooth);
 }
